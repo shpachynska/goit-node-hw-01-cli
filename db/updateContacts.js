5d6273b25abfbd328f95b37922dae0f000928342
Viktoriya Shpachynska <viktoriya.shpachynska@gmail.com>
2021-12-12 18:30:11 +0100

    deletes contacts update file

--- db/updateContacts.js
+++
@@ -1,8 +0,0 @@
-const fs = require("fs/promises");
-const contactsPath = require("../contacts");
-
-const updateContacts = async (contacts) => {
-  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
-};
-
-module.exports = updateContacts;
